b8658a6721760703fb809d02f41f05df7464e36f
Junewon Park <emptyzone@gmail.com>
2016-11-17 12:14:12 +1300

    - Add Laravel 5 IDE Helper Generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Loading the Laravel IDE Helper on non-production enviroments.
+    	if ($this->app->environment() !== 'production') {
+    		$this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+    	}
     }
 }
